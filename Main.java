00a981102ac47f1c54285859c1b6e4729cff5b0a
Ujwala4558 <112870659+Ujwala4558@users.noreply.github.com>
2023-04-21 19:50:20 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,21 @@
-import java.util.Scanner;
-public class Main {
- public static void main(String[] args) {
-  Scanner scanner = new Scanner(System.in);
-  System.out.println("Input the first number: ");
-  int n1 = scanner.nextInt();
-  System.out.println("Input the second number: ");
-  int n2 = scanner.nextInt();
-  int sum = n1 + n2;
-  int minus = n1 - n2;
-  int multiply = n1 * n2;
-  int subtract = n1 + n2;
-  int divide = n1 / n2;
-  int rnums = n1 % n2;
-  System.out.printf("Sum = %d\nMinus = %d\nMultiply = %d\nSubtract = %d\nDivide = %d\nRemainderOf2Numbers = %d\n ", sum, minus, multiply, subtract, divide, rnums);
- }
+
+class Main {
+  public static void main(String[] args) {
+
+    int[] num = {1, 2, 3, 4, 5};
+    int toFind = 3;
+    boolean found = false;
+
+    for (int n : num) {
+      if (n == toFind) {
+        found = true;
+        break;
+      }
+    }
+    
+    if(found)
+      System.out.println(toFind + " is found.");
+    else
+      System.out.println(toFind + " is not found.");
+  }
 }
